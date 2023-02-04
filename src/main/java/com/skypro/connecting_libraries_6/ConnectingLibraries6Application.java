db358352b492db5e00c94775eb11f0a0ea13f8a8
Polyxen1a <maslowa@inbox.ru>
2023-02-04 23:07:50 +0300

    Initial commit

---
+++ src/main/java/com/skypro/connecting_libraries_6/ConnectingLibraries6Application.java
@@ -0,0 +1,13 @@
+package com.skypro.connecting_libraries_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectingLibraries6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectingLibraries6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/connecting_libraries_6/ConnectingLibraries6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.connecting_libraries_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectingLibraries6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
